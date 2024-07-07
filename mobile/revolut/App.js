378d65de6418f157ba3aa492da3e5e7047afa489
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-07-07 21:50:27 +0530

    Revolut chart with d3 complete,

--- mobile/revolut/App.js
+++ mobile/revolut/App.js
@@ -1,8 +1,16 @@
-import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import Svg, { Circle, LinearGradient, Path, Stop } from "react-native-svg";
+import {
+  Animated,
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native";
+import Svg, { LinearGradient, Path, Stop } from "react-native-svg";
 import * as shape from "d3-shape";
-import { scaleTime, scaleLinear } from "d3-scale";
-
+import { scaleTime, scaleLinear, scaleQuantile } from "d3-scale";
+import { svgPathProperties } from "svg-path-properties";
+import { useCallback, useEffect, useRef, useState } from "react";
 const d3 = {
   shape,
 };
@@ -10,7 +18,11 @@ const d3 = {
 export default function App() {
   const height = 200;
   const verticalPadding = 5;
+  const cursorRadius = 10;
+  const labelWidth = 100;
+  const [x, setX] = useState(new Animated.Value(0));
   const { width } = Dimensions.get("window");
+
   const data = [
     { x: new Date(2018, 9, 1), y: 0 },
     { x: new Date(2018, 9, 16), y: 0 },
@@ -25,6 +37,9 @@ export default function App() {
   const scaleY = scaleLinear()
     .domain([0, 300])
     .range([height - verticalPadding, 0 + verticalPadding]);
+
+  const scaleLabel = scaleQuantile().domain([0, 200]).range([0, 100, 150, 200]);
+
   const line = d3.shape
     .line()
     .x((d) => {
@@ -43,6 +58,52 @@ export default function App() {
       height,
       width,
     },
+    cursor: {
+      width: 20,
+      height: 20,
+      borderRadius: 10,
+      borderColor: "#367be2",
+      borderWidth: 3,
+      backgroundColor: "white",
+    },
+    label: {
+      position: "absolute",
+      top: -45,
+      left: 0,
+      backgroundColor: "lightgray",
+      width: labelWidth,
+    },
+  });
+
+  const properties = new svgPathProperties(line);
+  const lineLength = properties.getTotalLength();
+
+  const cursorRef = useRef(null);
+  const labelRef = useRef(null);
+  console.log({ lineLength, width });
+
+  const moveCursor = useCallback((value) => {
+    const { x, y } = properties.getPointAtLength(lineLength - value);
+    cursorRef.current.setNativeProps({
+      top: y - cursorRadius,
+      left: x - cursorRadius,
+    });
+    const label = scaleLabel(scaleY.invert(y));
+    labelRef.current.setNativeProps({ text: `${label}$` });
+    labelRef.current.setNativeProps({ text: `${label}$` });
+  }, []);
+
+  useEffect(() => {
+    if (!cursorRef.current) return;
+    const id = x.addListener(({ value }) => moveCursor(value));
+    moveCursor(0);
+    return () => x.removeListener(id);
+  }, [moveCursor, x, cursorRef.current]);
+
+  const translateX = x.interpolate({
+    inputRange: [0, lineLength],
+    outputRange: [width - labelWidth, 0],
+    extrapolate: "clamp",
   });
 
   return (
@@ -63,7 +124,29 @@ export default function App() {
             d={`${line} L${width} ${height} L0 ${height}`}
             fill="url(#gradient)"
           />
+          <View ref={cursorRef} style={styles.cursor}></View>
         </Svg>
+        <Animated.View style={[styles.label, { transform: [{ translateX }] }]}>
+          <TextInput ref={labelRef} />
+        </Animated.View>
+        <Animated.ScrollView
+          style={{ ...StyleSheet.absoluteFill }}
+          contentContainerStyle={{ width: lineLength * 2 }}
+          // showsHorizontalScrollIndicator={false}
+          scrollEventThrottle={16}
+          bounces={false}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: { x },
+                },
+              },
+            ],
+            { useNativeDriver: true }
+          )}
+          horizontal
+        />
       </View>
     </SafeAreaView>
   );
