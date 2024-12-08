d8a1abb7a968510209196bc47a60876f2654b246
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-12-08 13:12:42 +0530

    Gravity fix,

--- mobile/reanim-pan-gesture/components/Pan.tsx
+++ mobile/reanim-pan-gesture/components/Pan.tsx
@@ -1,28 +1,53 @@
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
 } from "react-native-reanimated";
 
 const SIZE = 100;
+const CIRCLE_RADIUS = SIZE * 2;
 
 export default function Pan() {
   const translate = useSharedValue({ translateX: 0, translateY: 0 });
   const prevPosition = useSharedValue({ translateX: 0, translateY: 0 });
+  const { height, width } = Dimensions.get("screen");
+  const maxTranslateX = Number(width / 2) - Number(SIZE / 2);
+  const maxTranslateY = Number(height / 2) - Number(SIZE / 2);
 
   const panGestureEvent = Gesture.Pan()
-    .onStart((event) => {
+    .onStart(() => {
       prevPosition.value = {
-        translateX: event.translationX,
-        translateY: event.translationY,
+        translateX: translate.value.translateX,
+        translateY: translate.value.translateY,
       };
     })
     .onUpdate((event) => {
+      // Prevent going out of screen
+      let finalTranslateX = prevPosition.value.translateX + event.translationX;
+      if (Math.abs(finalTranslateX) >= maxTranslateX) {
+        finalTranslateX = finalTranslateX < 0 ? -maxTranslateX : maxTranslateX;
+      }
+
+      let finalTranslateY = prevPosition.value.translateY + event.translationY;
+      if (Math.abs(finalTranslateY) >= maxTranslateY) {
+        finalTranslateY = finalTranslateY < 0 ? -maxTranslateY : maxTranslateY;
+      }
+
       translate.value = {
-        translateX: prevPosition.value.translateX + event.translationX,
-        translateY: prevPosition.value.translateY + event.translationY,
+        translateX: finalTranslateX,
+        translateY: finalTranslateY,
       };
+    })
+    .onEnd(() => {
+      // Distance from center of the screen/square
+      const distance = Math.sqrt(
+        translate.value.translateX ** 2 + translate.value.translateY ** 2
+      );
+      if (distance < CIRCLE_RADIUS + SIZE / 2) {
+        translate.value = withSpring({ translateX: 0, translateY: 0 });
+      }
     });
 
   // const panGestureEventOld =
@@ -52,9 +77,11 @@ export default function Pan() {
 
   return (
     <View style={styles.container}>
-      <GestureDetector gesture={panGestureEvent}>
-        <Animated.View style={[styles.square, dStyle]}></Animated.View>
-      </GestureDetector>
+      <View style={styles.circle}>
+        <GestureDetector gesture={panGestureEvent}>
+          <Animated.View style={[styles.square, dStyle]}></Animated.View>
+        </GestureDetector>
+      </View>
     </View>
   );
 }
@@ -71,5 +98,14 @@ const styles = StyleSheet.create({
     height: SIZE,
     backgroundColor: "rgba(0, 0, 256, 0.5)",
     borderRadius: 20,
+  },
+  circle: {
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: CIRCLE_RADIUS,
+    borderWidth: 5,
+    borderColor: "rgba(0, 0, 256, 0.1)",
   },
 });
